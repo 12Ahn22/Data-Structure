581ef259567f463604de965603cd096c4d726be2
ayo <12ayo22@gmail.com>
2021-05-24 19:29:44 +0900

    bubble sort

---
+++ Sort/bubbleSort.js
@@ -0,0 +1,17 @@
+const arr = [6, 3, 8, 5, 2, 7, 4, 1];
+
+const bubbleSort = (array) => {
+  const length = array.length;
+
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = 0; j < length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+};
+console.log(bubbleSort(arr));
